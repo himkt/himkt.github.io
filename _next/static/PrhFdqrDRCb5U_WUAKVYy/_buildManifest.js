7ba488da8d2c1a4639fb943339d2d1ad4d166d3a
himkt <himkt@users.noreply.github.com>
2022-01-10 08:53:56 +0000

    deploy: c28790414444dc58cefda05566cccc299df5edcb

---
+++ _next/static/PrhFdqrDRCb5U_WUAKVYy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-25dafdb0c8601beb.js","static/chunks/pages/index-40e37f16493aa317.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
