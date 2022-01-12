481dc34454d534931ecf5a1aa54d1ea5b94a36b7
himkt <himkt@klis.tsukuba.ac.jp>
2022-01-12 11:23:35 +0900

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Profile from '../components/profile';
+import Head from 'next/head';
 
 const Index: NextPage = () => {
   return (
-    <div className="bg-gray-200">
-      <div className="container mx-auto">
-        <div className="flex place-content-center">
-          <Profile />
+    <>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <div className="bg-gray-200">
+        <div className="container mx-auto">
+          <div className="flex place-content-center">
+            <Profile />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/interest.tsx
+++ pages/interest.tsx
@@ -4,21 +4,24 @@ import Table from '../components/table';
 import useSWR, { Fetcher } from 'swr';
 import Paper from '../types/paper';
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const Interest: NextPage = () => {
   const appId =
     'AKfycbx1BW2FiLIOs-NPVDm-w2bBE2e_ci2KiqX0ezMqHKJbV1EtGOVldYzCQm6F6rZER6dj';
   const endpoint = `https://script.google.com/macros/s/${appId}/exec`;
 
+  const [searchKeyword, setSearchKeyword] = useState('');
   const fetcher: Fetcher<Paper[], string> = (url: string) =>
     fetch(url).then((res) => res.json());
   const { data, error } = useSWR(endpoint, fetcher);
   const isLoading = !error && !data;
 
-  const [searchKeyword, setSearchKeyword] = useState('');
-
   return (
     <>
+      <Head>
+        <title>Profile</title>
+      </Head>
       {isLoading && (
         <div className="w-full h-screen flex items-center justify-center">
           <h1 className="font-mono text-3xl animate-pulse">Loading...</h1>
